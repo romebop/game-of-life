c7eaddd12fe11d557c4cb33bd5493ea5f65fe4e4
Saerom Park <park.srome@gmail.com>
2022-06-01 12:03:33 -0500

    class refactor

--- life.js
+++ life.js
@@ -1,8 +1,9 @@
+// check with window
 document.addEventListener('DOMContentLoaded', () => {
   
-  const cellSize = 10;
+  const cellSize = 5;
+  const stepDuration = 50;
   const chanceOfLife = 0.1;
-  const intervalDuration = 30;
 
   const containerWidth = window.innerWidth;
   const containerHeight = window.innerHeight;
@@ -16,113 +17,129 @@ document.addEventListener('DOMContentLoaded', () => {
   const canvas = document.createElement('canvas');
   canvas.width = containerWidth;
   canvas.height = containerHeight;
-  document.body.prepend(canvas);
+  document.body.append(canvas);
 
-  let state = [...Array(numRows)].map(() => Array(numCols).fill(null));
-  if (Math.random() > 0.5) {
-    // random state
-    for (let y = 0; y < numRows; y++) {
-      for (let x = 0; x < numCols; x++) {
-        state[y][x] = +(Math.random() < chanceOfLife);
-      }
-    }
-  } else {
-    // gosper glider gun
-    let shape = [
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
-      [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ];
-    for (let y = 0; y < numRows; y++) {
-      for (let x = 0; x < numCols; x++) {
-        state[y][x] = (shape[y] && shape[y][x]) ? shape[y][x] : 0;
-      }
-    }
-  }
+  const game = new Game(canvas, numRows, numCols, cellSize, stepDuration, chanceOfLife);
+  game.start();
 
-  paintCanvas(canvas, cellSize, state);
-
-  let intervalId = setInterval(() => {
-    window.requestAnimationFrame(() => {
-      const oldState = state;
-      state = getNextState(state);
-      paintCanvas(canvas, cellSize, state, oldState);
-    });
-  }, intervalDuration)
-
-  document.addEventListener('keydown', (e) =>{
+  window.addEventListener('keydown', e => {
     if (e.code === 'Space') {
-      if (intervalId) {
-        clearInterval(intervalId);
-        intervalId = null;
+      if (typeof(game.intervalID) === 'number') {
+        game.stop();
       } else {
-        intervalId = setInterval(() => {
-          window.requestAnimationFrame(() => {
-            const nextState = getNextState(state);
-            paintCanvas(canvas, cellSize, state, nextState);
-            state = nextState;
-          });
-        }, intervalDuration)
+        game.start();
       }
     }
   });
 });
 
-function getNextState(state) {
-  const nextState = JSON.parse(JSON.stringify(state));
-  for (let y = 0; y < state.length; y++) {
-    for (let x = 0; x < state[0].length; x++) {
-      const numAliveNeighbors = getNumAliveNeighbors(state, x, y);
-      if (numAliveNeighbors === 2) {
-        nextState[y][x] = (state[y][x] === 2) ? 0 : state[y][x];
-      } else if (numAliveNeighbors === 3) {
-        nextState[y][x] = 1;
-      } else {
-        nextState[y][x] = (state[y][x] === 1) ? 2 : 0;
+class Game {
+
+  constructor(canvas, numRows, numCols, cellSize, stepDuration, chanceOfLife) {
+    this.canvas = canvas;
+    this.numRows = numRows;
+    this.numCols = numCols,
+    this.cellSize = cellSize;
+    this.stepDuration = stepDuration;
+    this.intervalID = null;
+
+    // init grid
+    this.grid = [...Array(numRows)].map(() => Array(numCols).fill(null));
+    for (let y = 0; y < numRows; y++) {
+      for (let x = 0; x < numCols; x++) {
+        this.grid[y][x] = new Cell(Math.random() < chanceOfLife ? 0 : 2);
+      }
+    }
+    for (let y = 0; y < numRows; y++) {
+      for (let x = 0; x < numCols; x++) {
+        const cell = this.grid[y][x];
+        for (let k = y - 1; k <= y + 1; k++) {
+          for (let j = x - 1; j <= x + 1; j++) {
+            if (
+              (j < 0 || j >= numCols || k < 0 || k >= numRows)
+              || (j === x && k === y)
+            ) continue;
+            const neighbor = this.grid[k][j];
+            cell.neighbors.push(neighbor);
+          }
+        }
+      }
+    }
+
+    this.paintCanvas();
+  }
+
+  start() {
+    this.intervalID = setInterval(() => {
+      window.requestAnimationFrame(() => {
+        this.getNextState();
+        this.paintCanvas();
+      });
+    }, this.stepDuration);
+  }
+
+  stop() {
+    clearInterval(this.intervalID);
+    // this.paintCanvas();
+    this.intervalID = null;
+  }
+
+  getNextState() {
+    for (let y = 0; y < this.numRows; y++) {
+      for (let x = 0; x < this.numCols; x++) { 
+        const cell = this.grid[y][x];
+        const numAliveNeighbors = cell.neighbors.reduce((a, c) => (
+          a + +(c.state === 0)
+        ), 0);
+        if (numAliveNeighbors === 2) {
+          cell.nextState = (cell.state === 1) ? 2 : cell.state;
+        } else if (numAliveNeighbors === 3) {
+          cell.nextState = 0;
+        } else {
+          cell.nextState = (cell.state === 0) ? 1 : 2;
+        }
+      }
+    }
+    for (let y = 0; y < this.numRows; y++) {
+      for (let x = 0; x < this.numCols; x++) {
+        const cell = this.grid[y][x];
+        cell.prevState = cell.state;
+        cell.state = cell.nextState;
+        cell.nextState = null;
       }
     }
   }
-  return nextState;
-}
 
-function getNumAliveNeighbors(state, x, y) {
-  let numAliveNeighbors = 0;
-  for (let k = y - 1; k <= y + 1; k++) {
-    for (let j = x - 1; j <= x + 1; j++) {
-      if (
-        (j < 0 || j >= state[0].length || k < 0 || k >= state.length)
-        || (j === x && k === y)
-      ) continue;
-      if (state[k][j] === 1) numAliveNeighbors++;
+  paintCanvas() {
+    const context = this.canvas.getContext('2d');
+    for (let y = 0; y < this.numRows; y++) {
+      for (let x = 0; x < this.numCols; x++) { 
+        const cell = this.grid[y][x];
+        if (cell.state === cell.prevState) continue;
+        if (cell.state === 0) context.fillStyle = '#0095ff';
+        if (cell.state === 1) context.fillStyle = '#bee3fe';
+        if (cell.state === 2) context.fillStyle = '#fff';
+                
+        // rounded rect
+        // const cell = new Path2D();
+        // cell.roundRect(x * cellSize, y * cellSize, cellSize, cellSize, [cellSize / 2]);
+        // context.fill(cell);
+        
+        // rect
+        context.fillRect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
+      }
     }
   }
-  return numAliveNeighbors;
+
 }
 
-function paintCanvas(canvas, cellSize, state, oldState = null) {
-  const context = canvas.getContext('2d');
-  for (let y = 0; y < state.length; y++) {
-    for (let x = 0; x < state[0].length; x++) {
-      if (oldState && oldState[y][x] === state[y][x]) continue;
-      if (state[y][x] === 0) context.fillStyle = '#fff';
-      if (state[y][x] === 1) context.fillStyle = '#0095ff';
-      if (state[y][x] === 2) context.fillStyle = '#bee3fe';
+class Cell {
 
-      // rounded rect
-      // const cell = new Path2D();
-      // cell.roundRect(x * cellSize, y * cellSize, cellSize, cellSize, [cellSize / 2]);
-      // context.fill(cell);
-      
-      // rect
-      context.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
-    }
+  constructor(state) {
+    this.state = state;
+    this.prevState = 2;
+    this.nextState = null;
+    this.neighbors = [];
   }
+
 }
